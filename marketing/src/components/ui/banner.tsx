32625fe9f7622edb2f575fdc41f75ca75683114a
Sebastian <se6359@gmail.com>
2024-10-26 21:36:24 +0200

    dict

--- marketing/src/components/ui/banner.tsx
+++ marketing/src/components/ui/banner.tsx
@@ -4,20 +4,20 @@ import IncomeCard from "../landing/cards/income";
 import GridBackground from "@/assets/svg/grid-bg";
 import { Dict } from "@/dict";
 
-export default function Banner({ dict: { title } }: { dict: Dict["banner"] }) {
+export default function Banner({ dict }: { dict: Dict["banner"] }) {
   return (
     <section className="py-24 sm:px-6">
       <div className="max-w-7xl mx-auto sm:rounded-xl bg-primary-dark py-40 sm:py-16 relative overflow-hidden px-6 sm:px-0">
         <div className="flex flex-col items-center gap-6 relative z-20">
           <h2 className="text-white text-3xl lg:text-4xl lg:leading-tight leading-tight font-black max-w-md lg:max-w-xl text-center [text-shadow:_4px_4px_0_rgb(11_60_64)]">
-            {title}
+            {dict.title}
           </h2>
           <div className="bg-primary/20 rounded-md p-1 flex items-center">
             <Link
               href="https://app.monfuse.com"
               className="bg-primary py-2.5 text-sm px-5 rounded-md text-white"
             >
-              Rozpocznij
+              {dict.button}
             </Link>
           </div>
         </div>
