8888db1d37a613b4f617d715065378aeaaeb5358
Filip <filfer05@gmail.com>
2025-05-27 15:56:10 +0200

    add google analytics & verification

--- marketing/src/app/shared-metadata.ts
+++ marketing/src/app/shared-metadata.ts
@@ -1,8 +1,17 @@
 import { LOCALES } from "@/lib/locales";
 import { Metadata } from "next";
 
+const GOOGLE_VERIFICATION_KEY = process.env.GOOGLE_VERIFICATION_KEY;
+
+  if (!GOOGLE_VERIFICATION_KEY) {
+    throw new Error("Environment variables missing: GOOGLE_VERIFICATION_KEY");
+  }
+
 const metadata: Omit<Metadata, "openGraph"> = {
   metadataBase: new URL("https://www.monfuse.com"),
+  verification: {
+    google: GOOGLE_VERIFICATION_KEY,
+  }
 };
 
 export const openGraph: Metadata["openGraph"] = {
